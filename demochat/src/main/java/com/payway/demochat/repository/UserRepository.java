939c9e4945f32c048ce8e3ae96ab694483b5846d
Otabek Karimov <89403346+creativeuz@users.noreply.github.com>
2023-08-04 16:01:09 +0500

    Delete demochat directory

--- demochat/src/main/java/com/payway/demochat/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.payway.demochat.repository;
-
-import com.payway.demochat.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
